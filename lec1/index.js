e498e57139131abc0485195d19595a7446cf86e0
Yash Gupta <76815371+yash2324@users.noreply.github.com>
2023-11-16 22:10:07 +0530

    day 2 work namaste

---
+++ lec1/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const heading1 = React.createElement("h1",{
+    id:"title",
+    hey : "heys",
+},"Namaste Everyone");
+const heading2 = React.createElement("h2",{
+    id : "title2",
+    hello : "hellew",
+},"Namaste bros");
+const container = React.createElement("div",{
+    id:"container"
+},[heading1,heading2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
