579169e273ae52c1beb2dff0cbb198f360d29bc9
Yash Gupta <76815371+yash2324@users.noreply.github.com>
2023-11-23 02:24:13 +0530

    changes to the food app

--- food/App.js
+++ food/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import logo from "./images/logo.png";
 const Title = () => {
   return (
-    <h1 id="title" key="h1">
-      Food Villa
-    </h1>
+    <a href="/">
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+    </a>
   );
 };
 const HeaderComponent = () => {
   return (
-    <div>
+    <div className="header">
       <Title />
       <div className="nav-items">
         <ul>
@@ -23,6 +26,44 @@ const HeaderComponent = () => {
     </div>
   );
 };
+const Body = () => {
+  return (
+    <div>
+      <RestrauntCard />
+    </div>
+  );
+};
+const RestrauntCard = () => {
+  return (
+    <div className="card">
+      <img
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf"
+        alt="image"
+      />
+      <h3>Burger King</h3>
+      <h4>Burgers,American</h4>
+      <h4>4.2 stars</h4>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer>
+      <h4>footer</h4>
+    </footer>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <HeaderComponent />,
+      <Body />
+      <Footer />
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeaderComponent />);
+root.render(<AppLayout />);
