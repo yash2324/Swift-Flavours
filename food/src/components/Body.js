b88d7ccf92090e1298af8e5362639bdad020cc2f
Yash <yg232004@gmail.com>
2024-01-14 21:43:52 +0530

    changes to food app

--- food/src/components/Body.js
+++ food/src/components/Body.js
@@ -18,7 +18,7 @@ const Body = () => {
   }, []);
   async function getRestaurants() {
     const data = await fetch(
-      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     console.log(json);
